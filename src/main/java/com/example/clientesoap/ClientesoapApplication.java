a949ccbf2e9b8e11a3f762afb6e63a5fe9d9291a
Luis Herrera <luis.herrera@tinet.cl>
2021-07-22 07:43:02 -0400

    se inicia el proyecto

---
+++ src/main/java/com/example/clientesoap/ClientesoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientesoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientesoap/ClientesoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
