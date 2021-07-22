f5d765f27eb18f2c28e7913afba612e7f0aca2bd
Luis Herrera <luis.herrera@tinet.cl>
2021-07-22 14:25:04 -0400

    se realiza test que prueba el servicio convertNumberToWord

--- src/test/java/com/example/clientesoap/soap/SoapClientTest.java
+++ src/test/java/com/example/clientesoap/soap/SoapClientTest.java
@@ -1,21 +1,18 @@
 package com.example.clientesoap.soap;
 
 import org.junit.jupiter.api.Assertions;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.stereotype.Component;
 
+/**
+ * Test class for SoapClient service.
+ */
 @SpringBootTest
 class SoapClientTest {
 
     @Autowired
-    SoapClient soapClient;
+    private SoapClient soapClient;
 
     /**
      * Test that perform the transformation of value to words.
